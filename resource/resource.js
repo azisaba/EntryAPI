04992cfe7e8c53d1b984f0ab68f6bf1e02e181e4
huda0209 <huda030209@gmail.com>
2022-07-18 23:38:06 +0900

    [add]コンフィグファイルを追加

---
+++ resource/resource.js
@@ -0,0 +1,68 @@
+/*
+created by huda0209
+EntryReSender
+
+resource.js
+ 
+ran by node.js
+2022-7-2
+*/
+
+/*
+DON'T TOUCH!!
+*/
+
+module.exports =  {
+    "setting.json" : {
+        pass : "./config/setting.json",
+        keys : {
+            NAME : {
+                canEmpty : false,
+                replace : false,
+                default : "EntryReSender"
+            },
+            MAIN_TOKEN : {
+                canEmpty : false,
+                replace : false,
+                default : ""
+            },
+            DEV_TOKEN : {
+                canEmpty : true,
+                replace : false,
+                default : ""
+            },
+            PREFIX : {
+                canEmpty : false,
+                replace : false,
+                default : "//"
+            },
+            COMMAND : {
+                canEmpty : false,
+                replace : false,
+                default : "ers"
+            },
+            PORT : {
+                canEmpty : false,
+                replace : false,
+                default : 3000
+            }
+        }
+    },
+
+    "RecruitmentsData.json" : {
+        pass : "./config/RecruitmentsData.json",
+        keys : {}
+    },
+    "OrgData.json" : {
+        pass : "./config/OrgData.json",
+        keys : {}
+    },
+    "guildData.json" : {
+        pass : "./config/guildData.json",
+        keys : {}
+    },
+    "recruitSystem.json" : {
+        pass : "./config/recruitSystem.json",
+        keys : {}
+    }
+}
