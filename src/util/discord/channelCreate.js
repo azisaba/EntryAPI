af2b2057ae34da8359542cedc0ce886a121679b7
huda0209 <huda030209@gmail.com>
2022-07-19 01:36:54 +0900

    [chg]アーカイブ期間をconfigから読みだすように変更

--- src/util/discord/channelCreate.js
+++ src/util/discord/channelCreate.js
@@ -5,16 +5,17 @@ EntryReSender for discord bot
  
 ran by node.js
 
-2022-7-18
+2022-7-19
 
 */
 
+const configManager = require("../../config/configManager");
 let DiscordClient = null;
 
 exports.createPublicThread = async(parentChannelId, threadName)=>{
     const thread = await(await DiscordClient.channels.fetch(parentChannelId)).threads.create({
 		name: threadName,
-		autoArchiveDuration: 10080,
+		autoArchiveDuration: configManager.getRecruitSystemData("archiveDuration"),
 		}
 	)
     return thread;
@@ -26,7 +27,7 @@ exports.createPublicThreadWithStartMessage = async(parentChannelId, threadName, 
     const message = await(await DiscordClient.channels.fetch(parentChannelId)).send(startMessage);
     const thread = await message.startThread({
         name: threadName,
-		autoArchiveDuration: 10080
+		autoArchiveDuration: configManager.getRecruitSystemData("archiveDuration")
     })
     return thread;
 }
