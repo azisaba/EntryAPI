32e92c20e3bc830571a156f0021098ed509c8886
huda0209 <huda030209@gmail.com>
2022-09-06 02:56:47 +0900

    [add]Punishmentクラスを追加

---
+++ src/structures/Punishment.js
@@ -0,0 +1,70 @@
+/*
+
+created by huda0209
+EntryReSender for discord bot
+
+ran by node.js
+
+2022-9-6
+
+*/
+
+'use strict'
+
+const MinecraftPlayer = require("./MinecraftPlayer");
+
+class Punishment{
+    constructor(id, target, reason, type, operator, startTime, endTime, server, extra, active) {
+        /**
+         * @type {Number} 処罰id
+         */
+        this.id = id;
+
+        /**
+         * @type {MinecraftPlayer} 処罰対象プレイヤー
+         */
+        this.target = target;
+
+        /**
+         * @type {MinecraftPlayer} 処罰の理由
+         */
+        this.reason = reason;
+
+        /**
+         * @type {String} 処罰の種類
+         */
+        this.type = type;
+
+        /**
+         * @type {MinecraftPlayer} 処罰執行プレイヤー
+         */
+        this.operator = operator;
+
+        /**
+         * @type {Date} 処罰された日時
+         */
+        this.startTime = startTime;
+
+        /**
+         * @type {Date} 処罰が終了する日時
+         */
+        this.endTime = endTime;
+
+        /**
+         * @type {String} 処罰されたサーバー
+         */
+        this.server = server;
+
+        /**
+         * @type {String} extra
+         */
+        this.extra = extra;
+
+        /**
+         * @type {Boolean} 処罰が有効か
+         */
+        this.active = active;
+    }
+}
+
+module.exports = Punishment;
