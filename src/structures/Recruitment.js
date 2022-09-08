aacc0cd49102c376eea2292ed0d55ea5c9292622
huda0209 <huda030209@gmail.com>
2022-09-08 17:08:37 +0900

    [add]募集組織のプロパティ追加/ setName,setMention,setReceptionのメソッドを追加

--- src/structures/Recruitment.js
+++ src/structures/Recruitment.js
@@ -5,14 +5,16 @@ EntryReSender for discord bot
 
 ran by node.js
 
-2022-9-6
+2022-9-8
 
 */
 
 'use strict'
 
+const {Organization} = require("./Organization");
+
 class Recruitment{
-    constructor(id, name, mention, reception) {
+    constructor(id, name, organization, mention, reception) {
         /**
          * @type {Number} 募集id
          */
@@ -29,11 +31,49 @@ class Recruitment{
         this.mention = mention;
 
         /**
+         * @type {Organization} 組織情報
+         */
+        this.organization = organization;
+
+        /**
          * @type {Boolean} 募集受付状態
          */
         this.reception = reception;
     }
+
+    /**
+     * 募集名を設定する。
+     * @param {String} name 募集名
+     * @return {void}
+     */
+    setName(name){
+        this.name = name;
+    }
+
+    /**
+     * 応募時のメンションを設定する。
+     * @param {Array<String>} mentionList メンションフォーマットの配列
+     * @return {void}
+     */
+    setMention(mentionList){
+        const DiscordIdRegexp = new RegExp("<@&?\d{18}>")
+        mentionList.forEach(value => {
+            if(!DiscordIdRegexp.test(value)){
+                throw new Error("Discord Mention Format Error");
+            }
+        })
+        this.mention = mentionList.join("");
+    }
+
+    /**
+     * 応募受付の状態を設定する。
+     * @param {Boolean} reception 募集受付状態
+     */
+    setReception(reception){
+        this.reception = reception;
+    }
+
 }
 module.exports = Recruitment;
 
-//setMention, setReception, delete, setName ...
+// delete ...
