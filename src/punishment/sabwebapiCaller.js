a09ad2199c272974c7a1028c1eb320fc7c807bd3
huda0209 <huda030209@gmail.com>
2022-09-09 23:58:50 +0900

    [chg/add]getPlayerData()をgetPlayerDataByUUID()に変更/getPlayerDataByName()を追加 getPlayerData()をuuidとmcidのいずれかで取得できるように分離

--- src/punishment/sabwebapiCaller.js
+++ src/punishment/sabwebapiCaller.js
@@ -21,7 +21,7 @@ const playerDataCache = new cacheManager();
 const punishmentCache = new cacheManager();
 const accountsCache = new cacheManager()
 
-exports.getPlayerData = async (uuid)=>{
+exports.getPlayerDataByUUID = async (uuid)=>{
     if(playerDataCache.exist(uuid)){
         return playerDataCache.get(uuid);
     }
@@ -45,6 +45,53 @@ exports.getPlayerData = async (uuid)=>{
         });
 }
 
+exports.getPlayerDataByName = async (name)=>{
+
+    /*
+    playerDataCache.
+    if(playerDataCache.exist(uuid)){
+        return playerDataCache.get(uuid);
+    }
+    */
+
+    const option = {
+        url : `${authData.url}/misc/search`,
+        headers: {
+            "Content-type": "application/json",
+            "X-SpicyAzisaBan-Session": await auth.getToken(),
+            "Origin" : "https://spicyazisaban.azisaba.net"
+        },
+        form: {
+            "query": "huda0209",
+            "type": "players,punishments"
+        }
+    }
+    return request.post(option)
+        .then(r => {
+            const players = (JSON.parse(r, "utf-8")).players;
+
+            if(players.length===0) return null;
+            const player = players.find(value=>{
+                return value.name===name;
+            })
+
+            /*フォーマットが本当は違う
+            misc/search
+            {
+                "name": "huda0209",
+                "uuid": "cb6e90f5-5a07-4089-824f-a3083b618a15",
+                "last_login": 000000,
+                "ip": "192.168.2.1"
+            }
+             */
+            playerDataCache.set(player.uuid, player, (new Date()).setMinutes((new Date()).getMinutes()+2))
+            return player
+        })
+        .catch(e => {
+            console.log(e);
+            return null;
+        });
+}
 
 exports.getPunishmentData = async (punishmentId)=>{
     if(punishmentCache.exist(punishmentId)){
