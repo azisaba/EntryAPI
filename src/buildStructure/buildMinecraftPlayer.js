f67ff236a510a95ce04b6bfcec50d19f0099050f
huda0209 <huda030209@gmail.com>
2022-09-19 03:42:46 +0900

    [add]MinecraftPlayerクラスを組み立てるコード追加

---
+++ src/buildStructure/buildMinecraftPlayer.js
@@ -0,0 +1,44 @@
+/*
+
+created by huda0209
+EntryReSender for discord bot
+
+ran by node.js
+
+2022-9-19
+
+*/
+
+'use strict'
+
+const sabAPI = require("../callApi/sabwebapiCaller");
+const azisabaAPI = require("../callApi/azisabaApiCaller");
+const MinecraftPlayer = require("../structures/MinecraftPlayer");
+
+/**
+ * uuidからMinecraftPlayerクラスを組み立てる
+ * @param {String}uuid
+ * @return {Promise<MinecraftPlayer>}
+ */
+exports.buildByUUID = async (uuid)=>{
+    try{
+        return new MinecraftPlayer(
+            (await azisabaAPI.getPlayerByUUID(uuid)).name,
+            uuid
+        );
+    }catch (e){}//uuidをgetできない為、playerデータのnameにアクセスできないのを無理やり回避
+
+}
+
+/**
+ * name(mcid)からMinecraftPlayerクラスを組み立てる
+ * @param {String}name
+ * @return {Promise<MinecraftPlayer>}
+ */
+exports.buildByName = async (name)=>{
+    const uuid = (await sabAPI.getPlayerDataByName(name)).uuid;
+    return new MinecraftPlayer(
+        name,
+        uuid
+    );
+}
