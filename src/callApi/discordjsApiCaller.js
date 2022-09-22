9b7de115558a76a1d9f2a9bec6fe81208070a6b4
huda0209 <huda030209@gmail.com>
2022-09-23 02:57:12 +0900

    [add]応スレッドを作るコード追加/メッセージを一瞬送信するコード追加 util/discordから移植

--- src/callApi/discordjsApiCaller.js
+++ src/callApi/discordjsApiCaller.js
@@ -1,6 +1,7 @@
 
 
-const {Client, GuildMember, AnyChannel} = require("discord.js")
+const {Client, GuildMember, TextChannel, ThreadChannel, AnyChannel} = require("discord.js")
+const configManager = require("../config/configManager");
 let client = null;
 /**
  * 初期化する
@@ -32,6 +33,47 @@ exports.getGuildMemberFromUserIdAndGuildId = async (guildId, userId) => {
  */
 exports.getChannelFromChannelId = async (channelId) =>{
     return (await client).channels.fetch(channelId);
+}
+
+/**
+ * 指定したチャンネルにパブリックスレッドを作成する
+ * @param {String}parentChannelId
+ * @param {String}threadName
+ * @return {Promise<ThreadChannel>}
+ */
+exports.createPublicThread = async(parentChannelId, threadName)=>{
+    const thread = await(await (await client).channels.fetch(parentChannelId)).threads.create({
+            name: threadName,
+            autoArchiveDuration: configManager.getRecruitSystemData("archiveDuration"),
+        }
+    )
+    return thread;
+}
+
+/**
+ * 指定したチャンネルに、指定した開始メッセージのパブリックスレッドを作成する
+ * @param {String}parentChannelId
+ * @param {String}threadName
+ * @param {String}startMessage
+ * @return {Promise<ThreadChannel>}
+ */
+exports.createPublicThreadWithStartMessage = async(parentChannelId, threadName, startMessage)=>{
+    const message = await(await (await client).channels.fetch(parentChannelId)).send(startMessage);
+    return await message.startThread({
+        name: threadName,
+        autoArchiveDuration: configManager.getRecruitSystemData("archiveDuration")
+    });
+}
+
+/**
+ * 一瞬メッセージを送信し、即座に削除する
+ * @param {String}channelId
+ * @param {String}content
+ * @return {Promise<void>}
+ */
+exports.momentMessage = async(channelId, content)=>{
+    const message = await(await DiscordClient.channels.fetch(channelId)).send(content);
+    message.delete();
 }
 
 exports.init = init;
