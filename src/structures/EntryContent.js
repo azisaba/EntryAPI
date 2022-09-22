b990986be68172b4d3f950aa6a69a975a404c88f
huda0209 <huda030209@gmail.com>
2022-09-23 02:51:03 +0900

    [add/chg]Embedのフィールド用の配列データを返すコード追加 / jobsとskillsに変更 sがないとpropertyErrorになる(リファレンスに合わせた)

--- src/structures/EntryContent.js
+++ src/structures/EntryContent.js
@@ -5,13 +5,24 @@ EntryReSender for discord bot
 
 ran by node.js
 
-2022-9-8
+2022-9-23
 
 */
 
 'use strict'
 
-const {Entry} = require("./Entry");
+const propertyName = {
+    reasonForApplying: "志望動機",
+    career: "運営経験",
+    participatingProject: "関わっているプロジェクト",
+    availableTimeOfDay: "一日に活動可能できる時間",
+    availableDaysOfWeek: "活動可能な曜日",
+    availableTimeZone: "活動可能な時間帯",
+    skills: "所持技術",
+    jobs: "希望職種",
+    playtime: "アジ鯖プレイ時間",
+    selfPR: "自己PR"
+}
 
 class EntryContent{
     constructor(reasonForApplying, career, participatingProject, availableTimeOfDay, availableDaysOfWeek, availableTimeZone, skills, jobs, playtime, selfPR) {
@@ -48,12 +59,12 @@ class EntryContent{
         /**
          * @type {String} 所持技術
          */
-        this.skill = skills;
+        this.skills = skills;
 
         /**
          * @type {String} 希望職種
          */
-        this.job = jobs;
+        this.jobs = jobs;
 
         /**
          * @type {String} アジ鯖プレイ時間
@@ -64,6 +75,19 @@ class EntryContent{
          * @type {String} 自己PR
          */
         this.selfPR = selfPR;
+    }
+
+    getEmbedFields(){
+        const fields = [];
+        for(const key in this.valueOf()){
+            fields.push(
+                {
+                    name : propertyName[key],
+                    value : this[key]
+                }
+            )
+        }
+        return fields;
     }
 }
 
