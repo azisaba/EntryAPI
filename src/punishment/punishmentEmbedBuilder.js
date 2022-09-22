548cd32e71333fc5b0a0e4d8951dd60d7fd77afe
huda0209 <huda030209@gmail.com>
2022-09-23 03:14:13 +0900

    [add]処罰情報の簡易版Embedを構築するコード追加

---
+++ src/punishment/punishmentEmbedBuilder.js
@@ -0,0 +1,33 @@
+/*
+
+created by huda0209
+EntryReSender for discord bot
+
+ran by node.js
+
+2022-9-23
+
+*/
+
+'use strict'
+
+const Discord = require("discord.js");
+
+
+/**
+ * 処罰情報のサマリーのEmbedを構築する
+ * @param {Array<Punishment>}punishments
+ * @return {{embeds: *[]}}
+ */
+exports.buildPlayerSummary = async (punishments)=>{
+    const list = (await Promise.all(punishments)).map(async punishment=>{
+            return `[#${(punishment.id)}](https://spicyazisaban.azisaba.net/punishments/view?id=${punishment.id}) ${punishment.type} ${await (await punishment).getReason()} 処罰日:<t:${Math.floor((await punishment).startTime.getTime()/1000)}:F> ${await (await punishment).isActive() ? "" : "解除済"}`;
+        })
+
+    return {embeds: [new Discord.MessageEmbed()
+            .setTitle(`${(await punishments[0]).target.username}の処罰サマリー`)
+            .setColor("#3C3C3C")
+            .setDescription((await Promise.all(list)).join("\n"))
+            .setFooter({"text" : "SpicyAzisaBan"})
+        ]}
+}
