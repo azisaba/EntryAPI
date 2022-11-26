4d81bbad4cf4998ed171f85e6c724634f2048d97
huda0209 <huda030209@gmail.com>
2022-11-27 00:06:47 +0900

    [add]組織説明フィールドを追加

--- src/structures/Organization.js
+++ src/structures/Organization.js
@@ -3,14 +3,14 @@
 const {TextChannel} = require("discord.js");
 
 class Organization{
-    constructor(name, sign, channel) {
+    constructor(name, sign, channel, description=null) {
         /**
          * @type {String} 組織名
          */
         this.name = name;
 
         /**
-         * @type {sign} 組織固有記号
+         * @type {String} 組織固有記号
          */
         this.sign = sign;
 
@@ -18,6 +18,11 @@ class Organization{
          * @type {TextChannel} 組織応募情報送信チャンネル
          */
         this.channel = channel;
+
+        /**
+         * @type {String} 組織説明
+         */
+        this.description = description
     }
 
     /**
@@ -27,6 +32,15 @@ class Organization{
     setChannel(channel){
         this.channel = channel;
     }
+
+    /**
+     * 組織説明を設定する。
+     * @param {String} description 組織説明
+     */
+    setDescription(description){
+        this.description = description;
+    }
+
     //delete ...
 }
 
