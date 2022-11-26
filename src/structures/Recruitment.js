ff35ff2af9bb34dc093cd4e14966278230c4cbea
huda0209 <huda030209@gmail.com>
2022-11-27 00:07:46 +0900

    [add]業務内容、募集人数、募集期間、採用期間のフィールドを追加

--- src/structures/Recruitment.js
+++ src/structures/Recruitment.js
@@ -3,7 +3,18 @@
 const {Organization} = require("./Organization");
 
 class Recruitment{
-    constructor(id, name, organization, mention, reception) {
+    /**
+     * @param {String} id 募集id
+     * @param {String} name 募集名
+     * @param {Organization} organization 組織情報
+     * @param {String} mention 応募受付時のメンション
+     * @param {Boolean} reception 募集委受付状態
+     * @param {String} jobDescription 業務内容
+     * @param {String} recruitmentNumbers 募集人数
+     * @param {String} recruitmentPeriod 募集期間
+     * @param {String} employmentPeriod 採用期間
+     */
+    constructor(id, name, organization, mention, reception, jobDescription, recruitmentNumbers, recruitmentPeriod, employmentPeriod) {
         /**
          * @type {String} 募集id
          */
@@ -28,6 +39,27 @@ class Recruitment{
          * @type {Boolean} 募集受付状態
          */
         this.reception = reception;
+
+        /**
+         * @type {String} 業務内容
+         */
+        this.jobDescription = jobDescription;
+
+        /**
+         * @type {String} 募集人数
+         */
+        this.recruitmentNumbers = recruitmentNumbers
+
+        /**
+         * @type {String} 募集期間
+         */
+        this.recruitmentPeriod = recruitmentPeriod;
+
+        /**
+         * @type {String} 採用期間
+         */
+        this.employmentPeriod = employmentPeriod;
+
     }
 
     /**
@@ -62,6 +94,37 @@ class Recruitment{
         this.reception = reception;
     }
 
+    /**
+     * 業務内容を設定する。
+     * @param {String} jobDescription
+     */
+    setJobDescription(jobDescription){
+        this.jobDescription = jobDescription;
+    }
+
+    /**
+     * 応募人数を設定する。
+     * @param {String} recruitmentNumbers
+     */
+    setRecruitmentNumbers(recruitmentNumbers){
+        this.recruitmentNumbers = recruitmentNumbers;
+    }
+
+    /**
+     * 募集期間を設定する。
+     * @param {String} recruitmentPeriod
+     */
+    setRecruitmentPeriod(recruitmentPeriod){
+        this.recruitmentPeriod = recruitmentPeriod;
+    }
+
+    /**
+     * 採用期間を設定する。
+     * @param {String} employmentPeriod
+     */
+    setEmploymentPeriod(employmentPeriod){
+        this.employmentPeriod = employmentPeriod;
+    }
 }
 module.exports = Recruitment;
 
